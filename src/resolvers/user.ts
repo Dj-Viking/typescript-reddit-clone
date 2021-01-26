b103cb4039b17e6439c154a6550c2c9d538cf324
DJ Viking <anders.swedishviking@gmail.com>
2021-01-25 22:16:03 -0500

    able to login a user and handle the login errors

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { MyContext } from 'src/types';
 import { User } from '../entities/User';
-import { Field, InputType, Resolver, Arg, Mutation, Ctx } from 'type-graphql';
+import { Field, InputType, Resolver, Arg, Mutation, Ctx, ObjectType } from 'type-graphql';
 import argon2 from 'argon2';
 
 @InputType()
@@ -9,6 +9,28 @@ class UsernamePasswordInput {
   username: string
   @Field()
   password: string
+}
+
+@ObjectType()
+class FieldError {
+
+  @Field()
+  field: String
+
+  @Field()
+  message: String
+
+}
+
+//user returned if worked
+// or error returned if error was there
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[]
+
+  @Field(() => User, { nullable: true })
+  user?: User
 }
 
 @Resolver()
@@ -46,6 +68,59 @@ export class UserResolver {
     } catch (error) {
       return console.log(error);
     }
+  }
+
+
+  /**
+   * @example
+    mutation{
+        login(options: {
+          username: "username",
+          password: "password"
+        }){
+          errors{
+            field
+            message
+          }
+          user{
+            id
+            username
+          }
+        }
+    }
+   */
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse | FieldError>{
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) 
+    {
+      return {
+        errors: [
+          {
+            field: 'credentials',
+            message: 'incorrect credentials'
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid)
+    {
+      return {
+        errors: [
+          {
+            field: "credentials",
+            message: "Incorrect credentials"
+          },
+        ],
+      };
+    }
+    return {
+      user
+    };
   }
 
 }
