c675d349857314f82d2205c0717a539d35e22fd9
DJ Viking <anders.swedishviking@gmail.com>
2021-01-25 19:54:02 -0500

    setting up apollo graphql server with express

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,44 @@
 import { MikroORM } from '@mikro-orm/core';
-import { Post } from './entities/Post';
+//import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 async function main(){
   //connect to database
   const orm = await MikroORM.init(mikroConfig);
   //console.log(orm);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, {
-    title: 'my first post'
-  });
-  //em is entity manager
-  await orm.em.persistAndFlush(post);
+  // const post = orm.em.create(Post, {
+  //   title: 'my first post'
+  // });
+  // //em is entity manager
+  // await orm.em.persistAndFlush(post);
 
   // const posts = await orm.em.find(Post, {});
   // console.log(posts);
-}
+
+  //create express app
+  const app = express();
+
+  //create apollo server
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log('\x1b[33m', `server started on localhost:${PORT}`, '\x1b[00m');
+  });
+
+};
 
 main().catch(e => console.log(e));
 console.log('hello world');
