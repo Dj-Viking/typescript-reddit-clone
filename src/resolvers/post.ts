c33907555a306a405c7274f745701c3cf58db622
DJ Viking <anders.swedishviking@gmail.com>
2021-01-25 20:53:30 -0500

    able to create and update a post by id

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Arg, Ctx, Int, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from 'type-graphql';
 import { Post } from '../entities/Post';
 import { MyContext } from '../types';
 
@@ -27,7 +27,7 @@ export class PostResolver {
    * @example 
    * //in graphql playground to create dynamic args
       query post($id: Int!){
-        post(id: $id){
+        post(identifier: $id){
           id
           title
           createdAt
@@ -35,7 +35,7 @@ export class PostResolver {
         }
       }
       {
-        post(id: 1) {
+        post(identifier: 1) {
           id
           title
           createdAt
@@ -46,8 +46,87 @@ export class PostResolver {
    */
   @Query(() => Post, { nullable: true })
   post(
-    @Arg('id', () => Int) id: number,
-    @Ctx() { em }: MyContext): Promise<Post | null> {
+    @Arg('identifier', () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
     return em.findOne(Post, { id });
+  }
+
+
+  /**
+   * @example 
+      mutation(
+        $title: String!
+      ) {
+        createPost(
+          title: $title
+        ) {
+          id
+          createdAt
+          updatedAt
+          title
+        }
+      }
+      createPost(
+        title: "example title arg"
+      ){
+        id
+        createdAt
+        updatedAt
+        title
+      }
+   */
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title') title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  /**
+   * 
+   * @example
+      mutation(
+        $title: String!,
+        $id: Int!
+      ) {
+        updatePost(
+          title: $title,
+          id: $id
+        ) {
+          id
+          createdAt
+          updatedAt
+          title
+        }
+      }
+      updatePost(
+        title: "example title arg",
+        id: 12
+      ){
+        id
+        createdAt
+        updatedAt
+        title
+      }
+   */
+  @Mutation(() => Post)
+  async updatePost(
+    @Arg('id', () => Int) id: number,
+    @Arg('title', () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== 'undefined') {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
   }
 }
