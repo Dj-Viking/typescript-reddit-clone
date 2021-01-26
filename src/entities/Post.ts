861c31911850c98766747d4851578c285a496917
DJ Viking <anders.swedishviking@gmail.com>
2021-01-25 20:17:15 -0500

    able to query the array of posts in graphql playground

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,17 +1,25 @@
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { ObjectType, Field, Int } from 'type-graphql';
 
+//this is now both an object type and an entity
+@ObjectType()
 @Entity()
 export class Post {
 
+  @Field(() => Int)
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: 'date' })
   createdAt = new Date();
 
+  @Field(() => String)
   @Property({ type: 'date', onUpdate: () => new Date() })
   updatedAt = new Date();
 
+  //can infer strings
+  @Field(() => String)
   @Property({ type: 'text' })
   title!: string;
 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 //import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
@@ -5,6 +6,7 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 async function main(){
   //connect to database
@@ -26,9 +28,10 @@ async function main(){
   //create apollo server
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false
-    })
+    }),
+    context: () => ({ em: orm.em })
   });
 
   apolloServer.applyMiddleware({ app });
