81662d729c3cc0f396b5fac40db38b69f19f1f98
DJ Viking <anders.swedishviking@gmail.com>
2021-01-25 20:29:05 -0500

    able to query a post by id in playground

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,53 @@
-import { Ctx, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Int, Query, Resolver } from 'type-graphql';
 import { Post } from '../entities/Post';
 import { MyContext } from '../types';
 
 @Resolver()
 export class PostResolver {
+  /**
+   * 
+   * @example
+    {
+      posts{
+        id
+        createdAt
+        updatedAt
+        title
+      }
+    }
+   */
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+
+  /**
+   *  
+   * @example 
+   * //in graphql playground to create dynamic args
+      query post($id: Int!){
+        post(id: $id){
+          id
+          title
+          createdAt
+          updatedAt
+        }
+      }
+      {
+        post(id: 1) {
+          id
+          title
+          createdAt
+          updatedAt
+        }
+      }
+
+   */
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg('id', () => Int) id: number,
+    @Ctx() { em }: MyContext): Promise<Post | null> {
+    return em.findOne(Post, { id });
   }
 }
