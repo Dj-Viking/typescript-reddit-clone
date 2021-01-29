238485a056be9046900ab86c261b046d3d3fae9f
DJ Viking <anders.swedishviking@gmail.com>
2021-01-28 19:44:30 -0500

    able to set a cookie in the browser

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -137,7 +137,7 @@ export class UserResolver {
   @Mutation(() => UserResponse)
   async login(
     @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse>{
     const user = await em.findOne(User, { username: options.username });
     if (!user) 
@@ -163,6 +163,9 @@ export class UserResolver {
         ],
       };
     }
+
+    req.session.userId = user.id;
+
     return {
       user
     };
