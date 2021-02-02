de1198a17124861cd821f2a5775023e055c95b90
Dj-Viking <anders.swedishviking@gmail.com>
2021-02-02 03:02:34 -0500

    setting up the register page input fields and validation

--- client/src/components/wrapper.tsx
+++ client/src/components/wrapper.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
 
-interface wrapperProps {
-    
+export type WrapperMaxWVariant = "small" | "regular" | "responsive"
+
+//creating option props with '?' 
+interface WrapperProps {
+  maxWVariant?: WrapperMaxWVariant
 }
 
-const Wrapper: React.FC<wrapperProps> = ({children}) => {
-    return (
-        <Box
-            maxW="300px"
-            mt="8"
-            mx="auto"
-            w="100%"
-        >
-            {children}
-        </Box>
-    );
+const Wrapper: React.FC<WrapperProps> = ({
+  children,
+  maxWVariant
+}) => {
+  function checkVariantProp(variant) {
+    if (variant === "regular") return "600px";
+    else if (variant === "small") return "300px";
+    else if (variant === "responsive") return "50%";
+  }
+  return (
+    <Box
+      maxW={checkVariantProp(maxWVariant)}
+      mt="8"
+      mx="auto"
+      w="100%"
+    >
+      {children}
+    </Box>
+  );
 }
 export default Wrapper;
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,80 +1,111 @@
 import React from 'react';
-import { 
-    Formik, 
-    Form, 
-    Field
-} from 'formik';
+import { Formik, Form, Field } from 'formik';
 import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react';
 import Wrapper from '../components/wrapper';
 
-interface registerProps {
-    
+interface RegisterProps {
+
 }
 
-
 //in next.js the name of the file in the pages folder becomes a route
-const Register: React.FC<registerProps> = ({}) => {
+const Register: React.FC<RegisterProps> = ({}) => {
 
-    function validateName(value) 
-    {
-        let error: string;
-        if (!value) error = "Name is required";
-        else if (value.length <= 3) error = "Name must be longer than 3 characters";
-        return error;
-    }
+  function validatePassword(value: string) {
+    let error: string;
+    if (!value) error = "Password is required";
+    else if (value.length <= 3) error = "Password must be longer than 3 characters";
+    return error;
+  }
+  function validateUsername(value: string) {
+    let error: string;
+    if (!value) error = "Username is required";
+    else if (value.length <= 3) error = "Username must be longer than 3 characters";
+    return error;
+  }
 
-    return (
-        <Wrapper>
-            <Formik
-                initialValues={{ name: "" }}
-                onSubmit={(values, actions) => {
-                    setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2))
-                        actions.setSubmitting(false)
-                    }, 200)
-                }}
-            >
-                {
-                    (props) => (
-                        <Form>
-                            <Field 
-                                name="name" 
-                                validate={validateName}
-                            >
-                                {({ field, form }) => (
-                                <FormControl isInvalid={form.errors.name && form.touched.name}>
+  return (
+    <Wrapper maxWVariant="responsive">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values, actions) => {
+          console.log(values);
+          //realistically do the ajax calls here
+          // and handle what happens before and after
+          // the data arrives
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            actions.setSubmitting(false)
+          }, 200)
+        }}
+      >
+        {
+          (props) => (
+            <Form>
 
-                                    <FormLabel htmlFor="name">
-                                        Username
-                                    </FormLabel>
-                                    <Input 
-                                        {...field} 
-                                        id="name" 
-                                        placeholder="Username" 
-                                    />
-
-                                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                                </FormControl>
-                                )}
-                            </Field>
-                                <Box
-                                    display="flex"
-                                    justifyContent="center"
-                                >
-                                    <Button
-                                        mt={4}
-                                        colorScheme="teal"
-                                        isLoading={props.isSubmitting}
-                                        type="submit"
-                                    >
-                                        Submit
-                                    </Button>
-                                </Box>
-                        </Form>
+              <Box mt={4}>
+                <Field 
+                  name="username" 
+                  validate={validateUsername}
+                >
+                  {
+                    ({ field, form }) => (
+                      <FormControl isInvalid={form.errors.username && form.touched.username}>
+                        <FormLabel htmlFor="username">Username</FormLabel>
+                        <Input
+                          {...field}
+                          type="username" 
+                          id="username" 
+                          placeholder="username" 
+                          autoComplete="off"
+                          required
+                        />
+                        <FormErrorMessage>{form.errors.username}</FormErrorMessage>
+                      </FormControl>
                     )
-                }
-            </Formik>
-        </Wrapper>
-    );
+                  }
+                </Field>
+              </Box>
+
+              <Box mt={4}>
+              <Field 
+                  name="password" 
+                  validate={validatePassword}
+                >
+                  {
+                    ({ field, form }) => (
+                      <FormControl isInvalid={form.errors.password && form.touched.password}>
+                        <FormLabel htmlFor="password">Password</FormLabel>
+                        <Input
+                          {...field}
+                          type="password" 
+                          id="password" 
+                          placeholder="password" 
+                          autoComplete="off"
+                          required
+                        />
+                        <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                      </FormControl>
+                    )
+                  }
+                </Field>
+              </Box>
+
+              <Box display="flex" justifyContent="center">
+                <Button
+                  mt={4}
+                  colorScheme="teal"
+                  isLoading={props.isSubmitting}
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              </Box>
+
+            </Form>
+          )
+        }
+      </Formik>
+    </Wrapper>
+  );
 }
 export default Register;
