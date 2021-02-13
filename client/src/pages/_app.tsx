2e79d3f68d1b4855d66210e34a7b67586cc2bf15
Dj-Viking <anders.swedishviking@gmail.com>
2021-02-12 19:55:29 -0500

    able to see posts on the front end

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,88 +1,18 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
 import React from 'react';
-import { Provider, createClient, dedupExchange, fetchExchange } from 'urql';
-import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
-
 import theme from '../theme';
-import { LoginMutation, LogoutMutation, MeDocument, MeQuery, RegisterMutation } from '../generated/graphql';
 
 function MyApp({ Component, pageProps }: any) {
-
-  //using this to properly cast types
-  function betterUpdateQuery<Result, Query>(
-    cache: Cache,
-    queryInput: QueryInput,
-    result: any,
-    fn: (r: Result, q: Query) => Query
-  ){
-    return cache.updateQuery(queryInput, (data) => fn(result, data as any) as any);
-  }
-
-  const client = createClient({
-    url: "http://localhost:4000/graphql",
-    //credentials for sending the cookie
-    fetchOptions: {
-      //but need to change the cors header on the server to the address
-      // of the client
-      credentials: "include"
-    },
-    exchanges: [ dedupExchange, cacheExchange({
-      //runs whenever these mutations run and updates the urql cache
-      updates: {
-        Mutation: 
-        {
-          login: function (_result, _args, cache, _info) {
-            betterUpdateQuery<LoginMutation, MeQuery>
-            (
-              cache,
-              { query: MeDocument },
-              _result,
-              function (result, query){
-                if (result.login.errors) return query; 
-                else return { me: result.login.user }
-              }
-            );
-          },
-          register: function (_result, _args, cache, _info) {
-            betterUpdateQuery<RegisterMutation, MeQuery>
-            (
-              cache,
-              { query: MeDocument },
-              _result,
-              function (result, query){
-                if (result.register.errors) return query; 
-                else return { me: result.register.user }
-              }
-            );
-          },
-          logout: function(_result, _args, cache, _info) {
-            // return null from the me query
-            betterUpdateQuery<LogoutMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              () => ({ me: null })
-            );
-          },
-        },
-      },
-    }), 
-    fetchExchange 
-  ]
-  });
-
   return (
-    <Provider value={client}>
-      <ChakraProvider resetCSS theme={theme}>
-        <ColorModeProvider
-          options={{
-            useSystemColorMode: true,
-          }}
-        >
-          <Component {...pageProps} />
-        </ColorModeProvider>
-      </ChakraProvider>
-    </Provider>
+    <ChakraProvider resetCSS theme={theme}>
+      <ColorModeProvider
+        options={{
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
   )
 };
 
---
+++ client/src/utils/createUrqlClient.ts
@@ -0,0 +1,59 @@
+import { cacheExchange } from "@urql/exchange-graphcache";
+import { dedupExchange, fetchExchange } from "urql";
+import { LoginMutation, MeQuery, MeDocument, RegisterMutation, LogoutMutation } from "../generated/graphql";
+import { betterUpdateQuery } from "./betterUpdateQuery";
+
+
+export const createUrqlClient = (_ssrExchange: any) => ({
+  url: "http://localhost:4000/graphql" || process.env.API_ENDPOINT as string,
+  fetchOptions: {
+    credentials: 'include' as const
+  },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      //runs whenever these mutations run and updates the urql cache
+      updates: 
+      {
+        Mutation: 
+        {
+          login: function (_result, _args, cache, _info) {
+            betterUpdateQuery<LoginMutation, MeQuery>
+            (
+              cache,
+              { query: MeDocument },
+              _result,
+              function (result, query){
+                if (result.login.errors) return query; 
+                else return { me: result.login.user }
+              }
+            );
+          },
+          register: function (_result, _args, cache, _info) {
+            betterUpdateQuery<RegisterMutation, MeQuery>
+            (
+              cache,
+              { query: MeDocument },
+              _result,
+              function (result, query){
+                if (result.register.errors) return query; 
+                else return { me: result.register.user }
+              }
+            );
+          },
+          logout: function(_result, _args, cache, _info) {
+            // return null from the me query
+            betterUpdateQuery<LogoutMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              function (){ return { me: null } }
+            );
+          },
+        },
+      },
+    }),
+    _ssrExchange,
+    fetchExchange
+  ]
+});
